67fb6ef09d04d88a29e7048daab09e09e0357c15
ben@bxdavies <ben@bxdavies.co.uk>
2021-05-13 16:46:04 +0100

    Fix: PEP 8 Styling

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 from flask_wtf import FlaskForm
 from wtforms import SelectField, TextField, FileField
 from wtforms import validators
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 from flask_wtf.file import FileField, FileRequired
 import os
 
@@ -20,15 +20,21 @@ app.config['SECRET_KEY'] = '12345'
 #########
 # Forms #
 #########
+
+
 class frmCreateProduct(FlaskForm):
     fieName = TextField(u'Product Name', validators=[DataRequired()])
-    fieType = TextField(u'Product Types or Size')
+    fieType = TextField(u'Product Types or Size', validators=[DataRequired()])
 
-
+##########
+# Errors #
+##########
 
 ##########
 # Routes #
 ##########
+
+
 @app.route('/')
 def home():
     return render_template('index.html', title='Home')
@@ -53,15 +59,16 @@ def create_products():
 
     return render_template('products/create.html', title='Create a Product', frmCreateProduct=frmCreateProduct())
 
+
 @app.route('/products/upload', methods=('GET', 'POST'))
 def upload_products():
     class frmUploadProduct(FlaskForm):
-        lisProducts = os.listdir('static/products') 
+        lisProducts = os.listdir('static/products')
         fieProduct = SelectField(u'Product', choices=lisProducts, validators=[DataRequired()], id='productName')
         fieType = SelectField(u'Type', validators=[DataRequired()], id='productType')
         fieFileName = TextField(u'File Name', validators=[DataRequired()])
         fieFile = FileField(u'File', validators=[FileRequired()])
-    
+
     if frmUploadProduct().is_submitted():
         strProduct = frmUploadProduct().fieProduct.data
         strType = frmUploadProduct().fieType.data
@@ -72,11 +79,12 @@ def upload_products():
         strFileName = strFileName.replace(' ', '-')
         filename, file_extension = os.path.splitext(file.filename)
         strFileName = strFileName + file_extension
-        file.save(os.path.join('static/products', strProduct, strType ,strFileName))
-    
+        file.save(os.path.join('static/products', strProduct, strType, strFileName))
+
     return render_template('products/upload.html', title='Upload a Product', frmUploadProduct=frmUploadProduct())
 
-@app.route('/upload',  methods=('GET', 'POST'))
+
+@app.route('/upload', methods=('GET', 'POST'))
 def upload_art():
     class frmUploadArt(FlaskForm):
         fieFileName = TextField(u'File Name', validators=[DataRequired()])
@@ -90,27 +98,30 @@ def upload_art():
         strFileName = strFileName.replace(' ', '-')
         filename, file_extension = os.path.splitext(file.filename)
         strFileName = strFileName + file_extension
-        file.save(os.path.join('static/uploads', strFileName))    
-    
+        file.save(os.path.join('static/uploads', strFileName))
+
     return render_template('upload.html', title='Upload Art', frmUploadArt=frmUploadArt())
 
-@app.route('/generate',  methods=('GET', 'POST'))
+
+@app.route('/generate', methods=('GET', 'POST'))
 def generate():
     lisFiles = []
-    for strFile in os.listdir('static/uploads') :
+    for strFile in os.listdir('static/uploads'):
         lisFiles.append(os.path.splitext(strFile)[0])
 
-    lisProducts = os.listdir('static/products') 
+    lisProducts = os.listdir('static/products')
+
     class frmFiles(FlaskForm):
         fieProduct = SelectField(u'Product', choices=lisProducts, validators=[DataRequired()])
         fieFile = SelectField(u'Working Image', choices=lisFiles, validators=[DataRequired()])
-    
+
     if frmFiles().is_submitted():
         session['product'] = frmFiles().fieProduct.data
         session['workingImage'] = frmFiles().fieFile.data
         return redirect(url_for('generator'))
 
     return render_template('generate.html', title='Generate Mockups', frmFiles=frmFiles())
+
 
 @app.route('/generator')
 def generator():
@@ -119,11 +130,14 @@ def generator():
 #######
 # Api #
 #######
+
+
 @app.route('/api/product/types')
 def api_product_types():
     strProduct = request.args.get('product', '01', type=str)
     lisTypes = os.listdir(f'static/products/{strProduct}')
     return jsonify(lisTypes)
+
 
 @app.route('/api/product/bases')
 def api_product_bases():
@@ -132,11 +146,13 @@ def api_product_bases():
     lisBases = []
     for strFile in os.listdir(f'static/products/{strProduct}/{strType}'):
         lisBases.append(os.path.splitext(strFile)[0])
-    
+
     return jsonify(lisBases)
 
 #########
 # Start #
 #########
+
+
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True)
