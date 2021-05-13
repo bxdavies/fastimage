9230329a00e65ec9925a2d4396be32bdc6044966
ben@bxdavies <ben@bxdavies.co.uk>
2021-05-13 11:40:39 +0100

    addded files

---
+++ app.py
@@ -0,0 +1,142 @@
+###########
+# Modules #
+###########
+from flask import Flask, json, render_template, request, session, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import SelectField, TextField, FileField
+from wtforms import validators
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired
+import os
+
+###################
+# Flask Variables #
+###################
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['SECRET_KEY'] = '12345'
+
+#########
+# Forms #
+#########
+class frmCreateProduct(FlaskForm):
+    fieName = TextField(u'Product Name', validators=[DataRequired()])
+    fieType = TextField(u'Product Types or Size')
+
+
+
+##########
+# Routes #
+##########
+@app.route('/')
+def home():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/products/create', methods=('GET', 'POST'))
+def create_products():
+    if frmCreateProduct().is_submitted():
+        strProductName = frmCreateProduct().fieName.data
+        strProductName = strProductName.strip()
+        strProductName = strProductName.lower()
+        strProductName = strProductName.replace(' ', '-')
+        os.mkdir(f'static/products/{strProductName}')
+
+        strProductTypes = frmCreateProduct().fieType.data
+        lisProductTypes = strProductTypes.split(',')
+        for strProductType in lisProductTypes:
+            strProductType = strProductType.strip()
+            strProductType = strProductType.lower()
+            strProductType = strProductType.replace(' ', '-')
+            os.mkdir(f'static/products/{strProductName}/{strProductType}')
+
+    return render_template('products/create.html', title='Create a Product', frmCreateProduct=frmCreateProduct())
+
+@app.route('/products/upload', methods=('GET', 'POST'))
+def upload_products():
+    class frmUploadProduct(FlaskForm):
+        lisProducts = os.listdir('static/products') 
+        fieProduct = SelectField(u'Product', choices=lisProducts, validators=[DataRequired()], id='productName')
+        fieType = SelectField(u'Type', validators=[DataRequired()], id='productType')
+        fieFileName = TextField(u'File Name', validators=[DataRequired()])
+        fieFile = FileField(u'File', validators=[FileRequired()])
+    
+    if frmUploadProduct().is_submitted():
+        strProduct = frmUploadProduct().fieProduct.data
+        strType = frmUploadProduct().fieType.data
+        strFileName = frmUploadProduct().fieFileName.data
+        file = frmUploadProduct().fieFile.data
+        strFileName = strFileName.strip()
+        strFileName = strFileName.lower()
+        strFileName = strFileName.replace(' ', '-')
+        filename, file_extension = os.path.splitext(file.filename)
+        strFileName = strFileName + file_extension
+        file.save(os.path.join('static/products', strProduct, strType ,strFileName))
+    
+    return render_template('products/upload.html', title='Upload a Product', frmUploadProduct=frmUploadProduct())
+
+@app.route('/upload',  methods=('GET', 'POST'))
+def upload_art():
+    class frmUploadArt(FlaskForm):
+        fieFileName = TextField(u'File Name', validators=[DataRequired()])
+        fieFile = FileField(u'File', validators=[FileRequired()])
+
+    if frmUploadArt().is_submitted():
+        strFileName = frmUploadArt().fieFileName.data
+        file = frmUploadArt().fieFile.data
+        strFileName = strFileName.strip()
+        strFileName = strFileName.lower()
+        strFileName = strFileName.replace(' ', '-')
+        filename, file_extension = os.path.splitext(file.filename)
+        strFileName = strFileName + file_extension
+        file.save(os.path.join('static/uploads', strFileName))    
+    
+    return render_template('upload.html', title='Upload Art', frmUploadArt=frmUploadArt())
+
+@app.route('/generate',  methods=('GET', 'POST'))
+def generate():
+    lisFiles = []
+    for strFile in os.listdir('static/uploads') :
+        lisFiles.append(os.path.splitext(strFile)[0])
+
+    lisProducts = os.listdir('static/products') 
+    class frmFiles(FlaskForm):
+        fieProduct = SelectField(u'Product', choices=lisProducts, validators=[DataRequired()])
+        fieFile = SelectField(u'Working Image', choices=lisFiles, validators=[DataRequired()])
+    
+    if frmFiles().is_submitted():
+        session['product'] = frmFiles().fieProduct.data
+        session['workingImage'] = frmFiles().fieProduct.data
+        return redirect(url_for('generator'))
+
+    return render_template('generate.html', title='Generate Mockups', frmFiles=frmFiles())
+
+@app.route('/generator')
+def generator():
+    return render_template('generator.html', title="Create T-Shirt", product=session['product'], workingImage=session['workingImage'])
+
+#######
+# Api #
+#######
+@app.route('/api/product/types')
+def api_product_types():
+    strProduct = request.args.get('product', '01', type=str)
+    lisTypes = os.listdir(f'static/products/{strProduct}')
+    return jsonify(lisTypes)
+
+@app.route('/api/product/bases')
+def api_product_bases():
+    strProduct = request.args.get('product', '01', type=str)
+    strType = request.args.get('type', '01', type=str)
+    lisBases = []
+    for strFile in os.listdir(f'static/products/{strProduct}/{strType}'):
+        lisBases.append(os.path.splitext(strFile)[0])
+    
+    return jsonify(lisBases)
+
+#########
+# Start #
+#########
+if __name__ == "__main__":
+    app.run(use_reloader=True, debug=True)
---
+++ static/js/generator.js
@@ -0,0 +1,188 @@
+/*
+    Event Handlers
+*/
+
+// Page Load
+window.addEventListener("load", function()
+{
+    var send = {product: product};
+    $.getJSON('/api/product/types', send, function(result)
+    {
+        for (i in result)
+        {
+            var myPargraph = document.createElement("p")
+
+            var myInput = document.createElement("input")
+            var myLabel = document.createElement("label")
+ 
+            myInput.setAttribute("type", "radio");
+            myInput.setAttribute("value", result[i]);
+            myInput.setAttribute("name", "productType")
+
+            myLabel.setAttribute("for", result[i]);
+            myLabel.innerHTML = result[i];
+
+            myPargraph.appendChild(myInput);
+            myPargraph.appendChild(myLabel);
+
+            document.getElementById("productType").prepend(myPargraph)
+        }
+    });
+});
+
+// Product Type Form Submit
+document.getElementById("productTypeSubmitButton").addEventListener("click", function()
+{
+    var productType = document.querySelector('input[name="productType"]:checked').value;
+
+    var send = {product: product, type: productType};
+    $.getJSON('/api/product/bases', send, function(result)
+    {
+       
+        for (i in result)
+        {
+            var myPargraph = document.createElement("p");
+
+
+            var myInput = document.createElement("input");
+            var myLabel = document.createElement("label");
+ 
+            myInput.setAttribute("type", "checkbox");
+            myInput.setAttribute("id", `${result[i]}Check`);
+            myInput.setAttribute("value", result[i]);
+            myInput.setAttribute("onclick", "productColourChecked(this.value)")
+            myLabel.setAttribute("for", result[i]);
+            myLabel.innerHTML = result[i];
+
+            myPargraph.appendChild(myInput);
+            myPargraph.appendChild(myLabel);
+
+            document.getElementById("productColours").appendChild(myPargraph)
+
+        };
+    });
+});
+
+// Move Image Up Button Press
+document.getElementById("moveImageUp").addEventListener("click", function() 
+{
+    moveup()
+});
+
+// Move Image Down Button Press
+document.getElementById("moveImageDown").addEventListener("click", function() 
+{
+    movedown()
+});
+
+// Move Image Left Button Press
+document.getElementById("moveImageLeft").addEventListener("click", function() 
+{
+    moveLeft()
+});
+
+// Move Image Right Button Press
+document.getElementById("moveImageRight").addEventListener("click", function() 
+{
+    moveright()
+});
+
+// Image Size Changed
+document.getElementById("imageSize").addEventListener('change', function (evt) {
+
+    var images = document.querySelectorAll(".overlayImage");
+    for (i = 0; i < images.length; i++)
+    {
+        images[i].style.width = this.value + "%";
+    }
+});
+
+// Save Images Button Press
+document.getElementById("saveImages").addEventListener("click", function() {
+    var div = document.getElementById('products');
+    var divs = div.getElementsByTagName('div');
+    console.log(divs)
+    for (i = 0; i < divs.length; i++)
+    {
+        var baseImage = divs[i].id.replace("Product", "")
+        domtoimage.toBlob(document.getElementById(divs[i].id)).then(function (blob) {
+                window.saveAs(blob, `${workingImage}-${product}-${baseImage}.png`);
+        });        
+    };
+});
+
+/* 
+    Functions
+*/
+function productColourChecked(baseImage)
+{
+    var myCheckbox = document.getElementById(`${baseImage}Check`);
+    if (myCheckbox.checked == true)
+    {
+        var productType = document.querySelector('input[name="productType"]:checked').value;
+        var myDiv = document.createElement('DIV')
+        myDiv.setAttribute("id", `${baseImage}Product`)
+        myDiv.setAttribute("class", "imageWrapper")
+        document.getElementById("products").appendChild(myDiv)
+        var filename = `/static/products/${product}/${productType}/${baseImage}.jpg`;
+        var filenameT = `/static/uploads/${workingImage}.png`;
+        var myImage = document.createElement("IMG");
+        var myImageT = document.createElement("IMG")
+        myImage.setAttribute("src", filename);
+        myImage.setAttribute("class", "baseImage")
+        myImageT.setAttribute("src", filenameT)
+        myImageT.setAttribute("class", "overlayImage")
+        myImageT.style.width = "50%";
+        myDiv.appendChild(myImage);
+        myDiv.appendChild(myImageT)
+    }
+    else
+    {
+        document.getElementById(`${baseImage}Product`).remove();
+    }
+
+};
+
+function moveup() {
+    var images = document.querySelectorAll(".overlayImage");
+    for (i = 0; i < images.length; i++)
+    {
+        style = window.getComputedStyle(images[i])
+        imageLocation = style.getPropertyValue('top');
+        newLocation =  parseInt( imageLocation, 10 ) - parseInt( 10, 10 ) + "px";
+        images[i].style.top = newLocation;
+    }
+};
+  
+function movedown() {
+    var images = document.querySelectorAll(".overlayImage");
+    for (i = 0; i < images.length; i++)
+    {
+        style = window.getComputedStyle(images[i])
+        imageLocation = style.getPropertyValue('top');
+        newLocation =  parseInt( imageLocation, 10 ) + parseInt( 10, 10 ) + "px";
+        images[i].style.top = newLocation;
+    }
+};
+  
+function moveright() {
+    var images = document.querySelectorAll(".overlayImage");
+    for (i = 0; i < images.length; i++)
+    {
+        style = window.getComputedStyle(images[i])
+        imageLocation = style.getPropertyValue('left');
+        newLocation =  parseInt( imageLocation, 10 ) + parseInt( 10, 10 ) + "px";
+        images[i].style.left = newLocation;
+    }
+};
+
+function moveLeft() {
+    var images = document.querySelectorAll(".overlayImage");
+    for (i = 0; i < images.length; i++)
+    {
+        style = window.getComputedStyle(images[i])
+        imageLocation = style.getPropertyValue('left');
+        newLocation =  parseInt( imageLocation, 10 ) - parseInt( 10, 10 ) + "px";
+        images[i].style.left = newLocation;
+    }
+};
